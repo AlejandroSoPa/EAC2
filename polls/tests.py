3f989f8d1cd13f8e334f37ef526fb08606f825ca
Alejandro Soldado <95918617+AlejandroSoPa@users.noreply.github.com>
2025-01-04 19:20:31 +0100

    Update tests.py

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
-# Create your tests here.
+class AdminPanelTests(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        opts = Options()
+        cls.selenium = WebDriver(options=opts)
+        cls.selenium.implicitly_wait(5)
+ 
+    @classmethod
+    def tearDownClass(cls):
+        # tanquem browser
+        # comentar la propera línia si volem veure el resultat de l'execució al navegador
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_admin_login(self):
+        # Accedim a la pàgina d'inici de sessió del panell d’administració
+        self.selenium.get(f"{self.live_server_url}/admin/login/")
+
+        # Localitzem els camps del formulari de login
+        username_input = self.selenium.find_element(By.NAME, "username")
+        password_input = self.selenium.find_element(By.NAME, "password")
+
+        # Introduïm les credencials i enviem el formulari
+        username_input.send_keys("isard")
+        password_input.send_keys("pirineus")
+        password_input.send_keys(Keys.RETURN)
+
+        # Comprovem que s’ha iniciat sessió correctament
+        self.selenium.find_element(By.XPATH, "//a[text()='Log out']")
+
+    def test_element_no_exists(self):
+        # Intentem trobar un element que no hi hauria de ser
+        try:
+            self.selenium.find_element(By.XPATH, "//a[text()='Element Inexistent']")
+            assert False, "Trobat element que NO hi ha de ser"
+        except NoSuchElementException:
+            pass
