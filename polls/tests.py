bd25b962b67efa59c2051cf0a565926c826c323e
Alejandro Soldado <95918617+AlejandroSoPa@users.noreply.github.com>
2025-01-04 19:59:48 +0100

    Update tests.py
    Nuevo test creando pregunta

--- polls/tests.py
+++ polls/tests.py
@@ -44,10 +44,29 @@ class AdminPanelTests(StaticLiveServerTestCase):
         # Comprovem que s’ha iniciat sessió correctament
         self.selenium.find_element(By.XPATH, "//button[@type='submit' and text()='Log out']")
 
-    def test_element_no_exists(self):
+    def test_non_existant_element(self):
         # Intentem trobar un element que no hi hauria de ser
         try:
             self.selenium.find_element(By.XPATH, "//a[text()='Element Inexistent']")
             assert False, "Trobat element que NO hi ha de ser"
         except NoSuchElementException:
             pass
+    
+    def test_create_poll(self):
+        # Accedim a la pàgina de creació de la enquesta
+        self.selenium.find_element(By.XPATH, "//a[@aria-describedby='polls-question']").click()
+
+
+        # Localitzem els camps del formulari
+        question_input = self.selenium.find_element(By.NAME, "question_text")
+        date_input = self.selenium.find_element(By.NAME, "pub_date_0")
+        time_input = self.selenium.find_element(By.NAME, "pub_date_1")
+
+        # Introduïm les dades i enviem el formulari
+        question_input.send_keys("Què és millor?")
+        date_input.send_keys("2021-01-01")
+        time_input.send_keys("12:00:00")
+        question_input.send_keys(Keys.RETURN)
+
+        # Comprovem que la nova enquesta ha estat creada
+        self.selenium.find_element(By.XPATH, "//a[text()='Què és millor?']")
