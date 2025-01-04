b904fd8020bf6b4d9a1984854a92892637c0e9ca
Alejandro Soldado <95918617+AlejandroSoPa@users.noreply.github.com>
2025-01-04 20:15:55 +0100

    Update tests.py

--- polls/tests.py
+++ polls/tests.py
@@ -45,8 +45,23 @@ class AdminPanelTests(StaticLiveServerTestCase):
         self.selenium.find_element(By.XPATH, "//button[@type='submit' and text()='Log out']")
 
     def test_create_poll(self):
-        self.selenium.find_element(By.XPATH, "//a[@href='/admin/polls/question/add/']").click()
+        # Accedim a la pàgina d'inici de sessió del panell d’administració
+        self.selenium.get(f"{self.live_server_url}/admin/login/")
 
+        # Localitzem els camps del formulari de login
+        username_input = self.selenium.find_element(By.NAME, "username")
+        password_input = self.selenium.find_element(By.NAME, "password")
+
+        # Introduïm les credencials i enviem el formulari
+        username_input.send_keys("isard")
+        password_input.send_keys("pirineus")
+        password_input.send_keys(Keys.RETURN)
+
+        # Comprovem que s’ha iniciat sessió correctament
+        self.selenium.find_element(By.XPATH, "//button[@type='submit' and text()='Log out']")
+
+        # Accedim a la pàgina de creació d'enquestes
+        self.selenium.find_element(By.XPATH, "//a[@href='/admin/polls/question/add/']").click()
 
         # Localitzem els camps del formulari
         question_input = self.selenium.find_element(By.NAME, "question_text")
