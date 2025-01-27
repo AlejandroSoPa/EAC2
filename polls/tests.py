68632bf3ad186e8b66c7d0497be2a5047a54cae3
AlejandroSoPa <alejandrosoldado03@gmail.com>
2025-01-27 18:40:49 +0100

    delete tests that dont need

--- polls/tests.py
+++ polls/tests.py
@@ -44,45 +44,6 @@ class AdminPanelTests(StaticLiveServerTestCase):
         # Comprovem que s’ha iniciat sessió correctament
         self.assertEqual( self.selenium.title , "Site administration | Django site admin" )
 
-    def test_create_poll(self):
-        # Creació del superusuari
-        user = User.objects.create_user("isard", "isard@isardvdi.com", "pirineus")
-        user.is_superuser = True
-        user.is_staff = True
-        user.save()
-
-        # Accedim a la pàgina d'inici de sessió del panell d’administració
-        self.selenium.get(f"{self.live_server_url}/admin/login/")
-
-        # Localitzem els camps del formulari de login
-        username_input = self.selenium.find_element(By.NAME, "username")
-        password_input = self.selenium.find_element(By.NAME, "password")
-
-        # Introduïm les credencials i enviem el formulari
-        username_input.send_keys("isard")
-        password_input.send_keys("pirineus")
-        password_input.send_keys(Keys.RETURN)
-
-        # Comprovem que s’ha iniciat sessió correctament
-        self.selenium.find_element(By.XPATH, "//button[@type='submit' and text()='Log out']")
-
-        # Accedim a la pàgina de creació d'enquestes
-        self.selenium.find_element(By.XPATH, "//a[@href='/admin/polls/question/add/']").click()
-
-        # Localitzem els camps del formulari
-        question_input = self.selenium.find_element(By.NAME, "question_text")
-        date_input = self.selenium.find_element(By.NAME, "pub_date_0")
-        time_input = self.selenium.find_element(By.NAME, "pub_date_1")
-
-        # Introduïm les dades i enviem el formulari
-        question_input.send_keys("Què és millor?")
-        date_input.send_keys("2021-01-01")
-        time_input.send_keys("12:00:00")
-        question_input.send_keys(Keys.RETURN)
-
-        # Comprovem que la nova enquesta ha estat creada
-        self.assertEqual( self.selenium.title , "Select question to change | Django site admin" )
-
     def test_eactest(self):
         # Creació del superusuari
         user = User.objects.create_user("isard", "isard@isardvdi.com", "pirineus")
@@ -135,14 +96,3 @@ class AdminPanelTests(StaticLiveServerTestCase):
         self.selenium.find_element(By.LINK_TEXT, "Change").click()
         choices = self.selenium.find_elements(By.CSS_SELECTOR, ".dynamic-choice_set .field-choice_text input")
         self.assertEqual(len(choices), 100, "El nombre d'opcions no és correcte")
-
-
-    def test_non_existant_element(self):
-        # Intentem trobar un element que no hi hauria de ser
-        try:
-            self.selenium.find_element(By.XPATH, "//a[text()='Element Inexistent']")
-            assert False, "Trobat element que NO hi ha de ser"
-        except NoSuchElementException:
-            pass
-    
-    
