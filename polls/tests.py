53b5616fcbd201797662d54949e4eab8eb44b2a7
Alejandro Soldado <95918617+AlejandroSoPa@users.noreply.github.com>
2025-01-27 21:58:46 +0100

    paginator

--- polls/tests.py
+++ polls/tests.py
@@ -94,6 +94,7 @@ class AdminPanelTests(StaticLiveServerTestCase):
         choices_link = self.selenium.find_element(By.XPATH, "//th[@id='polls-choice']/a")
         choices_link.click()
         # Contar todas las opciones disponibles en el modelo
+        paginator_text = self.selenium.find_element(By.CSS_SELECTOR, "p.paginator").text
         total_opciones = int(paginator_text.split(" ")[0])
         total_opciones_esperadas = sum(p["opciones"] for p in preguntas)
 
